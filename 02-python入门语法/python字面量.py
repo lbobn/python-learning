122525e2ac3bef28c711210917757e9bc07d542c
lubb <2820843645@qq.com>
2023-03-09 21:17:17 +0800

    Changes

---
+++ 02-python入门语法/python字面量.py
@@ -0,0 +1,31 @@
+# 你好
+"""
+多行注释
+1
+2
+"""
+print(12)
+print(13.14)
+print("你好")
+
+#
+int_type = type(12)
+float_type = type(13.14)
+String_type = type("你好")
+
+print(int_type)
+print(float_type)
+print(String_type)
+
+# 转换类型
+# 万物可转String
+num_str = str(11)
+print(type(num_str), num_str)
+
+i = int("123")
+print(i)
+
+f = float(12)
+print(f)
+int1 = int(12.56)
+print(int1)
