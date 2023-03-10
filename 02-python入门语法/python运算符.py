52187b6834799f0afbc3e6e028daee636b5b947e
lubb <2820843645@qq.com>
2023-03-10 22:07:00 +0800

    Changes

---
+++ 02-python入门语法/python运算符.py
@@ -0,0 +1,18 @@
+# 算数运算符
+print(1 + 1)  # 2
+print(2 - 1)  # 1
+print(4 / 2)  # 2.0
+print(4 * 2)  # 8
+print(9 // 4)  # 2     整除
+print(9 % 4)  # 1
+print(10 ** 2)  # 100    次方
+# 赋值运算符
+num = 1 + 2
+
+num += 1
+num -= 1
+num *= 1
+num /= 1
+num %= 1
+num //= 1
+num **= 1
