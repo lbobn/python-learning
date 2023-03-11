27a66baf03eb58fa2927f5af7ed77c427c696369
lubb <2820843645@qq.com>
2023-03-11 21:19:48 +0800

    Changes

---
+++ 03-python循环/猜数字案例.py
@@ -0,0 +1,18 @@
+"""
+猜数字,无限次机会
+"""
+import random
+
+r = random.randint(1, 100)
+
+flag = True
+while flag:
+    guess_num = int(input("请输入要猜的数字"))
+    if guess_num == r:
+        print("猜对了")
+        flag = False
+    else:
+        if guess_num > r:
+            print("猜大了")
+        else:
+            print("猜小了")
