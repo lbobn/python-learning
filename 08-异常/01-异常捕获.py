091d251ec01ad2b9f78c782e094f63f36f0127dd
lubb <2820843645@qq.com>
2023-03-31 22:19:50 +0800

    Changes

--- 08-异常/01-异常捕获.py
+++ 08-异常/01-异常捕获.py
@@ -12,3 +12,26 @@ try:
     print(name)
 except NameError as e:
     print("变量未定义异常")
+
+# 捕获多个异常
+try:
+    # print(1/0)
+    print(nnnn)
+except(NameError, ZeroDivisionError) as e:
+    print("出现了变量未定义或除以0的异常")
+
+# 未正确设置异常类型 将无法捕获异常
+
+
+# 捕获所有异常
+try:
+    print(12)
+    # print(1 / 0)
+    # open("D:/123.txt", "r")
+except Exception as e:
+    print("出现异常")
+else:
+    print("没有异常")
+finally:
+    print("无论有无异常,都会执行")
+#
