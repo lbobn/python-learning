0cbece73d7917c8e9f40b2513a995e8f5c87b2f9
lubb <2820843645@qq.com>
2023-04-02 20:19:15 +0800

    Changes

--- 可视化案例/01-json数据格式.py
+++ 可视化案例/01-json数据格式.py
@@ -3,7 +3,7 @@
 """
 
 import json
-
+# 转为json    dumps
 # 准备列表转为json
 data = [{"name": "张三", "age": 11}, {"name": "李四", "age": 19}]
 json_str = json.dumps(data, ensure_ascii=False)
@@ -13,3 +13,13 @@ print(type(json_str), " ", json_str)
 d = {"name": "wangwu", "age": 10}
 json_dumps = json.dumps(d)
 print(type(json_dumps), " ", json_dumps)
+
+# 转为Python      loads
+# 将json转为Python数据类型
+s = '[{"name": "张三", "age": 11}, {"name": "李四", "age": 19}]'
+loads = json.loads(s)
+print(type(loads), " ", loads)
+
+s1 = '{"name": "wangwu", "age": 10}'
+json_loads = json.loads(s1)
+print(type(json_loads), " ", json_loads)
