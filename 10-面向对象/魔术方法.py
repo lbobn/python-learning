f1939debf83a01ea6addb83350fde70455702570
lubb <2820843645@qq.com>
2023-04-06 21:45:01 +0800

    Changes

--- 10-面向对象/魔术方法.py
+++ 10-面向对象/魔术方法.py
@@ -11,7 +11,28 @@ class Student:
     def __str__(self):
         return f"Student类对象, name={self.name}, age={self.age}"
 
+    # __lt__   小于或大于的比较
+    def __lt__(self, other):
+        return self.age < other.age
 
-student = Student("李四", 11)
-print(student)
-print(str(student))
+    # __lt__   小于等于或大于等于的比较
+    def __le__(self, other):
+        return self.age <= other.age
+
+    # __eq__  等于的比较
+    def __eq__(self, other):
+        return self.age == other.age
+
+
+student1 = Student("李四", 11)
+student2 = Student("王五", 15)
+print(student1)
+print(str(student1))
+
+print(student1 > student2)
+print(student1 < student2)
+
+print(student1 <= student2)
+print(student1 >= student2)
+
+print(student1 == student2)  # ==号默认比较地址值
