cbbecb3c9de9db0e409b9468dbb41a9d2f916107
lubb <2820843645@qq.com>
2023-04-16 22:52:37 +0800

    Changes

--- 02-python入门语法/input语句.py
+++ 02-python入门语法/input语句.py
@@ -2,3 +2,18 @@ user_name = input("请输入用户名")
 user_type = input("请输入用户类型")
 
 print(f"您好:{user_name}, 您是尊贵的: {user_type}用户, 欢迎您的光临")
+
+
+def deleteDuplicates(self, head):
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
+
+    node = head
+    while node.next is not None and node is not None:
+        if node.val == node.next.val:
+            node.next = node.next.next
+        else:
+            node = node.next
+    return head
