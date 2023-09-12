d349147f60923baabd55c9f0cf987bf5d53d36c4
lubb <2820843645@qq.com>
2023-09-12 21:59:27 +0800

    Changes

--- 爬虫/解析/23-selenium基本使用.py
+++ 爬虫/解析/23-selenium基本使用.py
@@ -8,4 +8,26 @@ url = 'https://www.baidu.com/'
 
 browser.get(url)
 
-# sleep(100000)
+# 根据id来找对象
+button = browser.find_element_by_id('su')
+# print(button)
+
+# 根据标签属性找对象
+input = browser.find_element_by_name('wd')
+# print(input)
+
+# 根据Xpath语句获取对象
+input1 = browser.find_element_by_xpath('//input[@id="su"]')
+# print(input1)
+
+# 根据标签名字来获取对象
+inputs = browser.find_elements_by_tag_name('input')
+# print(inputs)
+
+# 根据bs4语法来获取对象，即CSS选择器
+button1 = browser.find_element_by_css_selector('#su')
+# print(button1)
+
+# 获取链接对象
+links = browser.find_element_by_link_text('图片')
+print(links)
